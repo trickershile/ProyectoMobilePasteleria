4d1a2a9cbab76368c3a0303bff194b43762e6cf4
Trickershile <victorspeed78@gmail.com>
2025-11-25 20:36:31 -0300

    se actualizo la ui y el proyecto

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -99,6 +99,7 @@ dependencies {
 
     // Testing
     testImplementation("junit:junit:4.13.2")
+    testImplementation("com.squareup.okhttp3:mockwebserver:4.12.0")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
